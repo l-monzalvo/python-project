7c81431302844dba73a3c8e3ec4a40cb981ca133
Leopoldo Monzalvo <lmonzalvo@gmail.com>
2025-03-11 22:29:45 -0600

    Added test cases

--- tests/test.py
+++ tests/test.py
@@ -1,2 +1,13 @@
-print("Testing")
 
+def test_greater():
+   num = 100
+   assert num > 100
+
+def test_greater_equal():
+   num = 100
+   assert num >= 100
+
+def test_less():
+   num = 100
+   assert num < 200
+   
